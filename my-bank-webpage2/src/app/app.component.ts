dda8642e24a7256b325abe263a17155057eabc64
orenWolinsky <oren.wolinsky@gmail.com>
2018-02-01 17:11:41 +0200

    adding angular project

---
+++ my-bank-webpage2/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Headers,Http,Response} from '@angular/http';
+import 'rxjs/Rx';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
+
