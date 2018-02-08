2a245e99170770cffe5bef548cab19e081fd09c6
Oren Wolinsky <Oren.Wolinsky@fundtech.isr>
2018-02-08 09:11:24 +0200

    adding pop ups to mop

--- my-bank-webpage3/src/app/users/navbar-component/navbar-component.component.ts
+++ my-bank-webpage3/src/app/users/navbar-component/navbar-component.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponentComponent implements OnInit {
 
+  public user:string = "oren";
+
   constructor() { }
 
   ngOnInit() {
