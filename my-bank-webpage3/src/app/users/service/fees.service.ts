4530adc1b972c5a59fad7f4e31da6278266c007a
Oren Wolinsky <Oren.Wolinsky@fundtech.isr>
2018-02-14 17:18:58 +0200

    update header and add picutres

--- my-bank-webpage3/src/app/users/service/fees.service.ts
+++ my-bank-webpage3/src/app/users/service/fees.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { HttpResponse } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpHeaders,HttpParams } from '@angular/common/http';
 import { inject } from '@angular/core/testing';
 import {TransferInfo} from "../json/transferInfo.json";
 import { Subject } from 'rxjs/Subject';
@@ -15,19 +15,35 @@ export class FeesService{
 
     public feesSubject:Subject<string[]> = new Subject();//should be array of feejsons //try working with task json
     public strArr: string[];
-
+    public json:string = `{
+        "localoffice" : "FIN",
+        "bdDebitamount" : 10,
+        "bdCreditamount" : 10,
+        "creditCurrency" : "EUR",
+      }`;
+    
+    public par = new HttpParams();
     constructor(private _httpService: HttpClient) {}
 
     public startFeesCalculation(info:TransferInfo): Observable<any[]> {
         console.log(info);
-        
-        return this._httpService.get<Stub[]>('https://nztodo.herokuapp.com/api/task/?format=json', {
+        let param:HttpParams = new HttpParams().set("localoffice","FIN").append("bdDebitamount","10").append("bdCreditamount","10").append("creditCurrency","EUR");
+        let head:HttpHeaders = new HttpHeaders()
+        .set("Access-Control-Allow-Headers","X-Requested-With,Content-Type")
+        .append("Access-Control-Allow-Methods","GET")
+        //.append("Access-Control-Allow-Origin","*")
+        .append("Access-Control-Allow-Origin","http://192.168.173.143:8080");
+//http://192.168.173.143:8080/CrunchifyTutorials/api/verify
+        return this._httpService.get('http://192.168.173.143:8080/CrunchifyTutorials/api/oren', {  //'https://nztodo.herokuapp.com/api/task/?format=json' 'http://192.168.169.59:8888/myapp/api/calculateFees'
             observe: 'response',
+            //headers: head,
+            //params: param
         })
-            .map((res: HttpResponse<Stub[]>) => {
-                const i:Stub = res.body[1] as Stub;
-                console.log(res.body[1]);
-                console.log(i.description);
+            .map((res) => {
+                console.log(res);
+                // const i:Stub = res.body[1] as Stub;
+                // console.log(res.body[1]);
+                // console.log(i.description);
                 // console.log(res.body[1].title); //playing with incoming json
                 // console.log(res.body[1]);
                 this.strArr = ["4.50","5.50","3.0"];
