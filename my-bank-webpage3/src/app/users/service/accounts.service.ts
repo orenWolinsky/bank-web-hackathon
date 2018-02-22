760814425086a218fe95cd1d4c1796e8ff12e7ea
Oren Wolinsky <Oren.Wolinsky@fundtech.isr>
2018-02-22 11:50:14 +0200

    production version 1.0

--- my-bank-webpage3/src/app/users/service/accounts.service.ts
+++ my-bank-webpage3/src/app/users/service/accounts.service.ts
@@ -2,7 +2,7 @@
 
 export class AccountsService{
 //6016 1331 9268 01
-    public accList:string[] = ["6016-1331-01 [USD]","6016-1331-02 [USD]","6016-1331-03 [EUR]"];
+    public accList:string[] = ["6016-1331-01 [SEK]","6016-1331-02 [USD]","6016-1331-03 [EUR]"];
     public defult:string = "My Accounts";
 
 }
